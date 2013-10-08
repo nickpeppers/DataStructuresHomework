e8eca3a3500fbb9d0c182f7d9d61dd85d1532910
Nick Peppers <nick.peppers@hitcents.com>
2013-10-08 09:57:10 -0500

    Finished homework I think

---
+++ Homework3/InfixToPostfix.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Homework3
+{
+    class InfixToPostfix
+    {
+        public static string ConvertToPostFix(string inFix)
+        {
+            StringBuilder postFix = new StringBuilder();
+
+            char incomingCharacter;
+
+            // Creates a new Stack
+            Stack<char> stack = new Stack<char>();
+
+            // Iterates characters in inFix
+            foreach (char character in inFix.ToCharArray())
+            {
+                // Checks if character is a number
+                if (Char.IsNumber(character))
+                {
+                    postFix.Append(character);
+                }
+                // Checks if parantheses
+                else if (character == '(')
+                {
+                    stack.Push(character);
+                }
+                // Removes all previous elements from Stack and puts them in front
+                else if (character == ')')
+                {
+                    incomingCharacter = stack.Pop();
+                    while (incomingCharacter != '(')
+                    {
+                        postFix.Append(incomingCharacter);
+                        incomingCharacter = stack.Pop();
+                    }
+                }
+                else
+                {
+                    // If and operator is found looks at the top of the stack
+                    if (stack.Count != 0 && Predecessor(stack.Peek(), character))
+                    {
+                        incomingCharacter = stack.Pop();
+                        // checks for while there are still incoming characters to be read
+                        while (Predecessor(incomingCharacter, character))
+                        {
+                            postFix.Append(incomingCharacter);
+
+                            // exits if there is nothing left in the stack
+                            if (stack.Count == 0)
+                            {
+                                break;
+                            }
+                            incomingCharacter = stack.Pop();
+                        }
+                        stack.Push(character);
+                    }
+                    else
+                    {
+                        //If Stack is empty or the operator has precedence
+                        stack.Push(character);
+                    }
+                }
+            }
+            while (stack.Count > 0)
+            {
+                incomingCharacter = stack.Pop();
+                postFix.Append(incomingCharacter);
+            }
+            return postFix.ToString();
+        }
+
+        // compares the two operators and decides which has precedence
+        private static bool Predecessor(char firstOperator, char secondOperator)
+        {
+            string operatorString = "(+-*/%";
+
+            int primary, secondary;
+
+            // "(" has less precedence
+            int[] precedence = { 0, 12, 12, 13, 13, 13 };
+
+            primary = operatorString.IndexOf(firstOperator);
+            secondary = operatorString.IndexOf(secondOperator);
+
+            // returns true if primary has greater precendence than secondary else returns false
+            return (precedence[primary] >= precedence[secondary]) ? true : false;
+        }
+    }
+}
--- Homework3/Main.cs
+++ Homework3/Main.cs
@@ -7,36 +7,50 @@ namespace Homework3
 	{
 		public static void Main (string[] args)
 		{
+            // Creation of linked listed
+            LinkedList<string> lList = new LinkedList<string>();
+            lList.AddFirst("hello");
+            lList.AddLast("world");
+            lList.AddLast("last");
+            lList.AddFirst("first");
 
-			LinkedList<string> lList = new LinkedList<string> ();
-			lList.AddFirst ("hello");
-			lList.AddLast ("world");
-			lList.AddLast ("last");
-			lList.AddFirst ("first");
+            // Creation of linked listed used to demonstrate circular
+            LinkedList<string> circularLList = new LinkedList<string>();
+            circularLList.AddFirst("two");
+            circularLList.AddLast("three");
+            circularLList.AddLast("four");
+            circularLList.AddFirst("one");
 
-			LinkedList<string> circularLList = new LinkedList<string>();
-			circularLList.AddFirst ("two");
-			circularLList.AddLast ("three");
-			circularLList.AddLast ("four");
-			circularLList.AddFirst ("one");
+            foreach (var node in lList)
+            {
+                Console.WriteLine(node);
+            }
 
-			Console.WriteLine(CircularLinkedList.PreviousOrLast(circularLList.First));
+            Console.WriteLine();
 
-			foreach (var node in lList) 
-			{
-				Console.WriteLine (node);
-			}
+            CheckForCircularList(lList, false);
+            CheckForCircularList(circularLList, true);
 
-			Console.WriteLine();
+            PrintInReverse(lList);
 
-			CheckForCircularList(lList, false);
-			CheckForCircularList(circularLList, true);
+            Console.WriteLine();
 
-			PrintInReverse(lList);
+            LongCheckForCircularList(lList, false);
+            LongCheckForCircularList(circularLList, true);
+
+            Console.WriteLine();
+
+            Console.WriteLine(InfixToPostfix.ConvertToPostFix("1+2-1*8+2*3+1"));
+            Console.WriteLine(InfixToPostfix.ConvertToPostFix("2+4*7+(3*4)"));
+            Console.WriteLine(InfixToPostfix.ConvertToPostFix("6+2-8*9"));
+
+            Console.ReadLine();
 		}
 
+        // Check for Circular list with runtime O(1) extra space
 		public static void CheckForCircularList (LinkedList<string> linkedList, bool simulateIfCircular)
 		{
+            // simulateIfCircular basically is used to tell my list to act like a circular one
 			if (!simulateIfCircular) 
 			{
 				if (linkedList.Last.Next != null) 
@@ -60,6 +74,49 @@ namespace Homework3
 				}
 			}
 		}
+
+        // Check for Circualar list with runtime O(N)
+        public static void LongCheckForCircularList(LinkedList<string> linkedList, bool simulateIfCircular )
+        {
+            // simulateIfCircular basically is used to tell my list to act like a circular one
+            var node = linkedList.First;
+            if (!simulateIfCircular)
+            {
+                for (int i = 0; i < linkedList.Count; i++)
+                {
+                    if (node.Next == null)
+                    {
+                        Console.WriteLine("List is not a cycle");
+                    }
+                    else if (node.Next != null & i != linkedList.Count - 1)
+                    {
+                        node = node.Next;
+                    }
+                    else
+                    {
+                        Console.WriteLine("List is a cycle");
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < linkedList.Count; i++)
+                {
+                    if (node.Next == null)
+                    {
+                        Console.WriteLine("List is not a cycle");
+                    }
+                    else if (node.Next != null & i != linkedList.Count - 1)
+                    {
+                        node = CircularLinkedList.NextOrFirst(node.Next);
+                    }
+                    else
+                    {
+                        Console.WriteLine("List is a cycle");
+                    }
+                }
+            }
+        }
 
 		public static void PrintInReverse(LinkedList<string> linkedList)
 		{
